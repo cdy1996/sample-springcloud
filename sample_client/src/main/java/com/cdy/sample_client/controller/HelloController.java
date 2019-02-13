8113a81d45e6f0888d288e1076c5a886fe4b8704
cdy1996 <13867868445@163.com>
2019-02-13 21:47:22 +0800

    说明: 启动nacos作为注册中心合配置中心, 使用新的SpringCloud-gateway代替原来的zuul,后续为实现gateway的动态配置

--- sample_client/src/main/java/com/cdy/sample_client/controller/HelloController.java
+++ sample_client/src/main/java/com/cdy/sample_client/controller/HelloController.java
@@ -1,13 +1,23 @@
 package com.cdy.sample_client.controller;
 
+import com.alibaba.nacos.api.annotation.NacosInjected;
+import com.alibaba.nacos.api.config.annotation.NacosValue;
+import com.alibaba.nacos.api.exception.NacosException;
+import com.alibaba.nacos.api.naming.NamingService;
+import com.alibaba.nacos.api.naming.pojo.Instance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 /**
  * todo
@@ -21,11 +31,24 @@ public class HelloController {
     @Autowired
     private RestTemplate restTemplate;
     
+    @NacosInjected
+    private NamingService namingService;
+    
     @Value("${client2.port}")
     private String port;
     
+    @NacosValue(value = "${client2.port}", autoRefreshed = true)
+    private boolean port2;
+    
     @Value("${client2.service.id}")
     private String client;
+    
+    
+    @RequestMapping(value = "/get", method = GET)
+    @ResponseBody
+    public List<Instance> get(@RequestParam String serviceName) throws NacosException {
+        return namingService.getAllInstances(serviceName);
+    }
     
     @RequestMapping("/hello")
     public String hello() {
--- sample_gateway/src/main/java/com/cdy/sample_zuul/SampleGatewayApplication.java
+++ sample_gateway/src/main/java/com/cdy/sample_zuul/SampleGatewayApplication.java
@@ -2,14 +2,13 @@ package com.cdy.sample_zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringCloudApplication
-@EnableZuulProxy
-public class SampleZuulApplication {
+public class SampleGatewayApplication {
     
+
     public static void main(String[] args) {
-        SpringApplication.run(SampleZuulApplication.class, args);
+        SpringApplication.run(SampleGatewayApplication.class, args);
     }
 }
 
